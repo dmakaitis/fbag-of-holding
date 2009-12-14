f8062e5b4bc5e544313ae7341c4c75ec801b012c
Feithar <dm_wowace@portkullis.com>
2009-12-14 23:40:24 +0000

    FBagOfHolding: - Added code to ensure that Outfitter is completely initialized before enabling the filter. Also, registers with Outfitter to receive events to force an update of the FBoH views when equipment sets are changed.
    git-svn-id: https://repos.wowace.com/wow/fbag-of-holding/trunk@168 81c27a14-0e1e-4fba-83cb-3e8c62f6afff

--- Modules/Outfitter/FBoH_Outfitter.lua
+++ Modules/Outfitter/FBoH_Outfitter.lua
@@ -7,12 +7,16 @@ if Outfitter then
 
 local outfitter = {};
 
+local outfitterInitialized = false;
+
 outfitter.name = "FBoH_Outfitter";
 
 outfitter.desc = "Outfitter";
 
 
 function outfitter.filter(itemProps, outfitName)
+	if(outfitterInitialized == false) then return false end;
+	
 	local outfitsForItem = Outfitter:GetOutfitsUsingItem(Outfitter:GetItemInfoFromLink(itemProps.itemLink))
 	if outfitsForItem then
 		if outfitName == "***ALL***" then return true end
@@ -31,6 +35,8 @@ function outfitter.getOptions()
 			value = "***ALL***",
 		},
 	};
+	
+	if(outfitterInitialized == false) then return rVal end;
 	
 	local outfits = Outfitter.Settings.Outfits;
 	if not outfits then return rVal end;
@@ -51,6 +57,22 @@ function outfitter.getOptions()
 end
 
 FBoH:RegisterProperty(outfitter);
+
+local
+function HandleOutfitterEvent(pEvent, pParameter1, pParameter2)
+	FBoH:Debug("Received Outfitter Event: " .. pEvent);
+	if(pEvent == "OUTFITTER_INIT") then
+		outfitterInitialized = true;
+	end;
+	FBoH:UpdateBags();
+end
+
+Outfitter:RegisterOutfitEvent("OUTFITTER_INIT", HandleOutfitterEvent);
+Outfitter:RegisterOutfitEvent("WEAR_OUTFIT", HandleOutfitterEvent);
+Outfitter:RegisterOutfitEvent("UNWEAR_OUTFIT", HandleOutfitterEvent);
+Outfitter:RegisterOutfitEvent("ADD_OUTFIT", HandleOutfitterEvent);
+Outfitter:RegisterOutfitEvent("DELETE_OUTFIT", HandleOutfitterEvent);
+Outfitter:RegisterOutfitEvent("EDIT_OUTFIT", HandleOutfitterEvent);
 
 end
 
